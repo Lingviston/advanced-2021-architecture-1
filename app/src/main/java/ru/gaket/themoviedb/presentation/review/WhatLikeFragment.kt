2a56ae7a93a6be54351bc012f8f0158439eb3667
Vlad Petrushkevich <vladpetrushkevich1999@gmail.com>
2021-09-19 17:26:58 +0300

    Add WhatDidLikeFragment and WhatDidNotLikeFragment

---
+++ app/src/main/java/ru/gaket/themoviedb/presentation/review/WhatLikeFragment.kt
@@ -0,0 +1,6 @@
+package ru.gaket.themoviedb.presentation.review
+
+import androidx.fragment.app.Fragment
+import ru.gaket.themoviedb.R
+
+class WhatLikeFragment : Fragment(R.layout.fragment_review_what_did_like)
---
+++ app/src/main/java/ru/gaket/themoviedb/presentation/review/WhatNotLikeFragment.kt
@@ -0,0 +1,6 @@
+package ru.gaket.themoviedb.presentation.review
+
+import androidx.fragment.app.Fragment
+import ru.gaket.themoviedb.R
+
+class WhatNotLikeFragment : Fragment(R.layout.fragment_review_what_did_like)
