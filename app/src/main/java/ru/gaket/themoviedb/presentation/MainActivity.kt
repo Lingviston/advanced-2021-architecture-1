3c7e29a1d3bbcab6be8f198a5566bc5ea9053847
Artur Badretdinov <artursletter@gmail.com>
2021-10-03 11:32:25 -0400

    Connect Navigation Component

--- app/src/main/java/ru/gaket/themoviedb/presentation/MainActivity.kt
+++ app/src/main/java/ru/gaket/themoviedb/presentation/MainActivity.kt
@@ -2,26 +2,19 @@ package ru.gaket.themoviedb.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.gaket.themoviedb.R
 import ru.gaket.themoviedb.core.navigation.MoviesScreen
 import ru.gaket.themoviedb.core.navigation.Navigator
+import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
-    @Inject
-    lateinit var navigator: Navigator
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        if (savedInstanceState == null) {
-            navigator.navigateTo(
-                screen = MoviesScreen(),
-                addToBackStack = false
-            )
-        }
+        setContentView(R.layout.main_activity)
     }
 }
--- app/src/main/java/ru/gaket/themoviedb/presentation/auth/view/AuthFragment.kt
+++ app/src/main/java/ru/gaket/themoviedb/presentation/auth/view/AuthFragment.kt
@@ -6,6 +6,7 @@ import androidx.annotation.StringRes
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import ru.gaket.themoviedb.R
