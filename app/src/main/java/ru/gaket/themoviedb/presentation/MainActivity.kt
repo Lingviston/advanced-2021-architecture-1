e3a7c41912ccfb194b595bc07f55fa2668912e3c
Artur Badretdinov <artursletter@gmail.com>
2021-10-03 11:33:15 -0400

    Remove redundant imports

--- app/src/main/java/ru/gaket/themoviedb/presentation/MainActivity.kt
+++ app/src/main/java/ru/gaket/themoviedb/presentation/MainActivity.kt
@@ -2,13 +2,8 @@ package ru.gaket.themoviedb.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.gaket.themoviedb.R
-import ru.gaket.themoviedb.core.navigation.MoviesScreen
-import ru.gaket.themoviedb.core.navigation.Navigator
-import timber.log.Timber
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
