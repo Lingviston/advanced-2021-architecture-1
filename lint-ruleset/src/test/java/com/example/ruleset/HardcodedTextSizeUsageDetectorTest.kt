5ce64038d8212ac99dbc48638346a9e583fff535
Vladislav Ermolin <v_ermolin@flo.health>
2021-11-06 21:24:48 +0300

    - Cover custom Lint rule with tests.

---
+++ lint-ruleset/src/test/java/com/example/ruleset/HardcodedTextSizeUsageDetectorTest.kt
@@ -0,0 +1,27 @@
+@file:Suppress("UnstableApiUsage")
+
+package com.example.ruleset
+
+import com.android.tools.lint.checks.infrastructure.TestFiles.xml
+import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import org.junit.jupiter.api.Test
+
+internal class HardcodedTextSizeUsageDetectorTest {
+
+    @Test
+    fun `given resource is used in textSize when detector applies then HardcodedTextSizeUsageIssue is not reported`() {
+        val layout = xml(
+            "res/layout/activity_main.xml",
+            """
+                <TextView xmlns:android="http://schemas.android.com/apk/res/android"
+                    android:textSize="@dimen/textSizeBig"/>
+            """
+        ).indented()
+
+        lint()
+            .files(layout)
+            .issues(HardcodedTextSizeUsageIssue)
+            .run()
+            .expectClean()
+    }
+}
