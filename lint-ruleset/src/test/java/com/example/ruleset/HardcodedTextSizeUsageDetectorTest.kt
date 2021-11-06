35174a1f2bc281edb7569d286b2c9b720aa93774
Vladislav Ermolin <v_ermolin@flo.health>
2021-11-06 21:25:16 +0300

    - Example of the "issue found" scenario test.

--- lint-ruleset/src/test/java/com/example/ruleset/HardcodedTextSizeUsageDetectorTest.kt
+++ lint-ruleset/src/test/java/com/example/ruleset/HardcodedTextSizeUsageDetectorTest.kt
@@ -24,4 +24,21 @@ internal class HardcodedTextSizeUsageDetectorTest {
             .run()
             .expectClean()
     }
+
+    @Test
+    fun `given hardcoded SP value is used in textSize when detector applies then HardcodedTextSizeUsageIssue is reported`() {
+        val layout = xml(
+            "res/layout/activity_main.xml",
+            """
+                <TextView xmlns:android="http://schemas.android.com/apk/res/android"
+                    android:textSize="18sp"/>
+            """
+        ).indented()
+
+        lint()
+            .files(layout)
+            .issues(HardcodedTextSizeUsageIssue)
+            .run()
+            .expectWarningCount(1)
+    }
 }
